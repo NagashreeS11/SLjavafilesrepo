a9717f4860bbde273aabf427eae0b3a8f622fa49
nagashrs <nagashree.s11@gmail.com>
2020-10-30 04:00:14 +0000

    adding code through eclipse terminal

--- SLfirstjavaprj/src/Inheritance/CreateNewLoan.java
+++ SLfirstjavaprj/src/Inheritance/CreateNewLoan.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CreateNewLoan {
 	public static void main(String args[]) {
 		Scanner s1 = new Scanner(System.in);
-	     System.out.println("Enter loan type (1 for Home Loan | 2 for Car Loan) :");
+	     System.out.println("Enter loan type (1 for Home Loan1 | 2 for Car Loan1) :");
 		 String opt = s1.nextLine();
 		 if (opt.matches("1")) {
 			 HomeLoan h1 = new HomeLoan("Jay Chand",200000, 120, "345609" );
